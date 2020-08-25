b7bab091c4af311caa7aeb5c7d467ee73290e0af
harashbarik3 <harashbarik3@gmail.com>
2020-08-25 15:51:15 +0530

    added MVC

--- Demo.BookStore/Demo.BookStore/Startup.cs
+++ Demo.BookStore/Demo.BookStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
 
 namespace Demo.BookStore
 {
@@ -16,6 +17,7 @@ namespace Demo.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +28,36 @@ namespace Demo.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my first middleware\n");
+            //    await next();
+            //    await context.Response.WriteAsync("hello from my first Response\n");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my second middleware\n");
+            //    await next();
+            //    await context.Response.WriteAsync("hello from my second response\n");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my Third middleware\n");
+                
+            //});
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapDefaultControllerRoute();
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/harash", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Harash");
                 });
             });
         }
